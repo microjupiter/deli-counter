2db6602a9f08054c7275613637ba84a9ada9f245
Kyle Wilson <kyle@hinterlandstills.com>
2020-11-12 11:19:53 -0600

    commit tree 4b6f62268fad9c6c890b69dd449babe9b43b4b35

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(deli)
+    line_array = []
+    if deli.length == 0
+        puts "The line is currently empty."
+    else
+        deli.each.with_index(1) do |name, index|
+            line_array.push ("#{index}. #{name}")
+        end
+        puts "The line is currently: #{line_array.join(" ")}"
+    end
+end
+
+def take_a_number(deli, name)
+    deli.push(name)
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(array)
+    if array.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
